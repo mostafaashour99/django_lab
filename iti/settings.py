b2d20cc3fde61af8a4ab6580d709a2a1cc885f07
mostafaashour99 <mostafa01284248878@gmail.com>
2022-09-06 22:28:27 +0200

    second lab

--- iti/settings.py
+++ iti/settings.py
@@ -75,12 +75,22 @@ WSGI_APPLICATION = 'iti.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_database',
+        'USER': 'mostafa',
+        'PASSWORD': 'mostafa',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
---
+++ products/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from products.models import products
+
+class ProuctForm(forms.ModelForm):
+    class Meta:
+        model = products
+        fields = '__all__'
